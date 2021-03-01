359ddd3b0803c2904b5ff543b33d1ac0f2487a46
miranda-company <rodolfo.mcompany@gmail.com>
2021-03-01 19:05:19 +0100

    WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
+let requestURL = 'http://localhost:3000/js/dynamic-data.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    console.log("JSON load complete");
+    const dynamicData = request.response;
+    populateFeaturedWork(dynamicData);
+}
+
+function populateFeaturedWork(obj) {
+    const name = obj['name'];
+    console.log(name);
+}
+
 window.onload = function () {
 
 }
