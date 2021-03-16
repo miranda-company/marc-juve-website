9f9537f5fb05857e87c9ccf05896213b2d9c9c1c
miranda-company <rodolfo.mcompany@gmail.com>
2021-03-16 09:21:15 +0100

    Dynamic data WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,16 @@
 var App = {
     showConsole: true,
     transitionSpeed: 0.38,
-    dynamicData: null,
+    isDynamicData: false,
 
     init: function () {
         this.cons("App initialized, console is active");
-        this.getDynamicData();
-
+        gsap.to(".overlay", { duration: App.transitionSpeed, autoAlpha: 0 });
+        Home.init();
     },
 
     cons: function (msg) {
         this.showConsole ? console.log("- " + msg) : console.log("Console innactive");
-    },
-
-    getDynamicData: function () {
-        let requestURL = 'http://localhost:3000/js/dynamic-data.json';
-        let request = new XMLHttpRequest();
-        request.open('GET', requestURL);
-        request.responseType = 'json';
-        request.send();
-
-        request.onload = function () {
-            App.cons("JSON load complete");
-            App.dynamicData = request.response;
-            Home.init();
-        }
     }
 }
 
@@ -48,75 +34,71 @@ var Home = {
     featuredWork: {
         mainContainer: $("#js-featured-work"),
         grid: $("#featured-work-grid"),
-        cardsDynamicData: [],
+        cardData: [],
+        cards: null,
 
         init: function () {
-            this.populate.search();
+            this.getData();
+            this.buildCards();
+            this.getCards();
+            this.addCardListeners();
+        },
+
+        getData: function () {
+            // Search for projects to be featured
+            App.cons("Searching for featured projects");
+            let results = DynamicData.search("projects", "featured", true);
+
+            Home.featuredWork.cardData = results;
+            App.cons("Found " + Home.featuredWork.cardData.length + " projects to be featured");
+        },
+
+        buildCards: function () {
+            App.cons("Building featured work grid");
+            for (let i = 0; i < Home.featuredWork.cardData.length; i++) {
+                let card = document.createElement("div");
+                card.setAttribute("id", Home.featuredWork.cardData[i].id);
+                card.className = "card fw-card";
+                Home.featuredWork.grid.append(card);
+
+                let cardContent = document.createElement("div");
+                cardContent.className = "card-content";
+                card.append(cardContent);
+
+                let thumb = document.createElement("div");
+                thumb.className = "thumb";
+                cardContent.append(thumb);
+
+                let thumbImg = document.createElement("img");
+                thumbImg.className = "thumb-image";
+                thumbImg.setAttribute("src", Home.featuredWork.cardData[i].thumbUrl);
+                thumb.append(thumbImg);
+
+                let cardOverlay = document.createElement("div");
+                cardOverlay.className = "card-overlay";
+                cardContent.append(cardOverlay);
+
+                let cardTitle = document.createElement("h2");
+                cardTitle.className = "card-title";
+                cardTitle.innerHTML = Home.featuredWork.cardData[i].title;
+                cardContent.append(cardTitle);
+
+                let cardSubtitle = document.createElement("h3");
+                cardSubtitle.className = "card-subtitle";
+                cardSubtitle.innerHTML = Home.featuredWork.cardData[i].subtitle;
+                cardContent.append(cardSubtitle);
+            }
+        },
+
+        getCards: function () {
+            Home.featuredWork.cards = $(".fw-card");
         },
 
         addCardListeners: function () {
-            App.cons("Adding event listeners to FW cards" + Home.featuredWork.cardsDynamicData.length);
-            $.each($(".js-fw-card"), function (i) {
+            App.cons("Adding event listeners to " + Home.featuredWork.cardData.length + " featured work cards");
+            $.each(Home.featuredWork.cards, function (i) {
                 $(this).on("click", Home.featuredWork.cardHandler);
             });
-        },
-
-        populate: {
-            search: function () {
-                App.cons("Searching for featured projects");
-                let dynamicData = App.dynamicData["projects"];
-                let results = [];
-                let searchField = "featured";
-                let searchVal = true;
-
-                for (let i = 0; i < dynamicData.length; i++) {
-                    if (dynamicData[i][searchField] == searchVal) {
-                        results.push(dynamicData[i]);
-                    }
-                }
-
-                Home.featuredWork.cardsDynamicData = results;
-                App.cons("Found " + Home.featuredWork.cardsDynamicData.length + " projects to be featured");
-                this.build();
-            },
-
-            build: function () {
-                App.cons("Building featured work grid");
-                for (let i = 0; i < Home.featuredWork.cardsDynamicData.length; i++) {
-                    let card = document.createElement("div");
-                    card.setAttribute("id", Home.featuredWork.cardsDynamicData[i].id);
-                    card.className = "card js-fw-card";
-                    Home.featuredWork.grid.append(card);
-
-                    let cardContent = document.createElement("div");
-                    cardContent.className = "card-content";
-                    card.append(cardContent);
-
-                    let thumb = document.createElement("div");
-                    thumb.className = "thumb";
-                    cardContent.append(thumb);
-
-                    let thumbImg = document.createElement("img");
-                    thumbImg.className = "thumb-image";
-                    thumbImg.setAttribute("src", Home.featuredWork.cardsDynamicData[i].thumbUrl);
-                    thumb.append(thumbImg);
-
-                    let cardOverlay = document.createElement("div");
-                    cardOverlay.className = "card-overlay";
-                    cardContent.append(cardOverlay);
-
-                    let cardTitle = document.createElement("h2");
-                    cardTitle.className = "card-title";
-                    cardTitle.innerHTML = Home.featuredWork.cardsDynamicData[i].title;
-                    cardContent.append(cardTitle);
-
-                    let cardSubtitle = document.createElement("h3");
-                    cardSubtitle.className = "card-subtitle";
-                    cardSubtitle.innerHTML = Home.featuredWork.cardsDynamicData[i].subtitle;
-                    cardContent.append(cardSubtitle);
-                }
-                Home.featuredWork.addCardListeners();
-            }
         },
 
         cardHandler: function (event) {
@@ -146,42 +128,32 @@ var Project = {
     closeBtn: $(".close-btn"),
 
     init: function (projectID) {
+        console.clear();
         this.id = projectID;
-        this.populate.search();
+        this.getData();
+        this.build();
     },
 
-    populate: {
-        search: function () {
-            let dynamicData = App.dynamicData['projects'];
-            App.cons("Searching for '" + Project.id + "' inside dynamic data");
-            let results = [];
-            let searchField = "id";
-            let searchVal = Project.id;
+    getData: function () {
+        // Search for projects to be featured
+        App.cons("Searching for '" + Project.id + "' data");
+        let results = DynamicData.search("projects", "id", Project.id);
 
-            for (let i = 0; i < dynamicData.length; i++) {
-                if (dynamicData[i][searchField] == searchVal) {
-                    results.push(dynamicData[i]);
-                    Project.data = dynamicData[i];
-                    App.cons("Project '" + Project.data.id + "' was found and has been populated");
-                }
-            }
+        Project.data = results;
+        App.cons("Found project data for " + Project.data[0]["title"]);
+    },
 
-            this.assign();
-        },
+    build: function () {
+        Project.title.text(Project.data[0]["title"]);
+        Project.subtitle.text(Project.data[0]["subtitle"]);
+        Project.category.text(Project.data[0]["category"]);
 
-        assign: function () {
-            Project.title.text(Project.data.title);
-            Project.subtitle.text(Project.data.subtitle);
-            Project.category.text(Project.data.category);
+        let videoSrc = Project.data[0]["videoUrl"];
+        let vimeoOptions = "?autoplay=1&color=bb0207&title=0&byline=0&portrait=0";
+        Project.video.attr("src", videoSrc + vimeoOptions);
+        Project.videoWrapper.addClass(Project.data[0]["aspectRatio"]);
 
-            var videoSrc = Project.data.videoUrl;
-            var vimeoOptions = "?autoplay=1&color=bb0207&title=0&byline=0&portrait=0";
-            Project.video.attr("src", videoSrc + vimeoOptions);
-            Project.videoWrapper.addClass(Project.data.aspectRatio);
-
-            Project.open();
-        },
-
+        Project.open();
     },
 
     open: function () {
@@ -190,7 +162,7 @@ var Project = {
         this.mainContainer.removeClass("display-none");
         this.closeBtn.on("click", this.closeHandler);
 
-        // Fade in
+        // Project fade in
         gsap.to(".overlay", {
             duration: App.transitionSpeed, autoAlpha: 0
         });
@@ -200,7 +172,7 @@ var Project = {
         App.cons("Closing project");
         Project.mainContainer.addClass("display-none");
         Home.unfreeze();
-        Project.videoWrapper.removeClass(Project.data.aspectRatio);
+        Project.videoWrapper.removeClass(Project.data[0]["aspectRatio"]);
         Project.isOpen = false;
         Project.data = null;
         Project.video.attr("src", "");
@@ -210,7 +182,45 @@ var Project = {
     }
 }
 
+var DynamicData = {
+    data: null,
+
+    init: function () {
+        this.get();
+    },
+
+    get: function () {
+        let requestURL = 'http://localhost:3000/js/dynamic-data.json';
+        let request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+
+        request.onload = function () {
+            App.cons("Dynamic data was loaded correctly");
+            DynamicData.data = request.response;
+            App.init();
+        }
+    },
+
+    search: function (obj, field, value) {
+        App.cons("Searching inside '" + obj + "' > " + field + " > " + value);
+        let results = [];
+        let dynamicObject = DynamicData.data[obj];
+        let searchField = field;
+        let searchVal = value;
+
+        for (let i = 0; i < dynamicObject.length; i++) {
+            if (dynamicObject[i][searchField] == searchVal) {
+                results.push(dynamicObject[i]);
+            }
+        }
+
+        return results;
+    }
+
+}
+
 window.onload = function () {
-    App.init();
-    gsap.to(".overlay", { duration: App.transitionSpeed, autoAlpha: 0 });
+    DynamicData.init();
 }
