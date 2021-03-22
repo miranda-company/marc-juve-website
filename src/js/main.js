975e13be9fc185508f7bb889171f655a7038f86b
miranda-company <rodolfo.mcompany@gmail.com>
2021-03-22 13:59:13 +0100

    Polishing

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 var App = {
     showConsole: true,
-    transitionSpeed: 0.38,
+    transitionSpeed: 0.6,
     isDynamicData: false,
 
     init: function () {
@@ -40,8 +40,6 @@ var Home = {
         init: function () {
             this.getData();
             this.buildCards();
-            this.getCards();
-            this.addCardListeners();
         },
 
         getData: function () {
@@ -56,38 +54,12 @@ var Home = {
         buildCards: function () {
             App.cons("Building featured work grid");
             for (let i = 0; i < Home.featuredWork.cardData.length; i++) {
-                let card = document.createElement("div");
-                card.setAttribute("id", Home.featuredWork.cardData[i].id);
-                card.className = "card fw-card";
-                Home.featuredWork.grid.append(card);
-
-                let cardContent = document.createElement("div");
-                cardContent.className = "card-content";
-                card.append(cardContent);
-
-                let thumb = document.createElement("div");
-                thumb.className = "thumb";
-                cardContent.append(thumb);
-
-                let thumbImg = document.createElement("img");
-                thumbImg.className = "thumb-image";
-                thumbImg.setAttribute("src", Home.featuredWork.cardData[i].thumbUrl);
-                thumb.append(thumbImg);
-
-                let cardOverlay = document.createElement("div");
-                cardOverlay.className = "card-overlay";
-                cardContent.append(cardOverlay);
-
-                let cardTitle = document.createElement("h2");
-                cardTitle.className = "card-title";
-                cardTitle.innerHTML = Home.featuredWork.cardData[i].title;
-                cardContent.append(cardTitle);
-
-                let cardSubtitle = document.createElement("h3");
-                cardSubtitle.className = "card-subtitle";
-                cardSubtitle.innerHTML = Home.featuredWork.cardData[i].subtitle;
-                cardContent.append(cardSubtitle);
+                var newCard = new Card(Home.featuredWork.grid, Home.featuredWork.cardData[i].id, "fw-card", Home.featuredWork.cardData[i].title, Home.featuredWork.cardData[i].subtitle, Home.featuredWork.cardData[i].category, Home.featuredWork.cardData[i].thumbUrl);
+                newCard.build();
             }
+
+            this.getCards();
+            this.addCardListeners();
         },
 
         getCards: function () {
@@ -106,17 +78,17 @@ var Home = {
         },
 
         hoverHandler: function (event) {
-            App.cons("Hover on " + this.id);
             let cardOverlay = $(this).find(".card-overlay");
             let cardTitle = $(this).find(".card-title");
             let cardSubtitle = $(this).find(".card-subtitle");
             let thumbImage = $(this).find(".thumb-image");
-            gsap.to([cardOverlay, cardTitle, cardSubtitle], { autoAlpha: 1 });
-            gsap.to([thumbImage], { duration: 0.8, scale: 1.03 });
+            gsap.set([cardOverlay, cardTitle, cardSubtitle], { autoAlpha: 0 });
+            gsap.to([cardOverlay], { duration: 0.4, autoAlpha: 1, ease: Power1.easeInOut });
+            gsap.to([cardTitle, cardSubtitle], { delay: 0.2, duration: 0.8, autoAlpha: 1, ease: Power1.easeInOut });
+            gsap.to([thumbImage], { duration: 0.4, scale: 1.02 });
         },
 
         outHandler: function (event) {
-            App.cons("Out");
             let cardOverlay = $(this).find(".card-overlay");
             let cardTitle = $(this).find(".card-title");
             let cardSubtitle = $(this).find(".card-subtitle");
@@ -143,35 +115,53 @@ var Project = {
     data: null,
     isOpen: false,
     mainContainer: $(".project-wrap"),
-    video: null,
+    videoSection: null,
     videoWrapper: null,
-    title: $(".project-title"),
-    subtitle: $(".project-subtitle"),
-    category: $(".project-category"),
-    description: $(".about-text"),
-    festivalsContainer: $(".project-festivals"),
-    festivals: $(".festivals"),
-    poster: $(".poster"),
-    posterImage: $(".poster-image"),
-    pressKit: $(".project-press-kit"),
-    pressKitLink: $(".press-kit-link"),
-    closeBtn: $(".close-btn"),
+    video: null,
+    title: null,
+    subtitle: null,
+    category: null,
+    role: null,
+    description: null,
+    festivalsSection: null,
+    festivals: null,
+    posterSection: null,
+    posterImage: null,
+    pressKitSection: null,
+    pressKitAnchorTag: null,
+    rwSection: null,
+    rwGrid: null,
+    rwCards: null,
     relatedWork: [],
-    relatedWorkGrid: $("#related-work-grid"),
-    relatedWorkCards: null,
+    closeBtn: $(".close-btn"),
 
     init: function (projectID) {
         console.clear();
         App.cons("Opening project...")
         this.id = projectID;
-        this.video = $(".project-video");
+
+        this.videoSection = $(".project-hero-video");
         this.videoWrapper = $(".video-wrapper");
+        this.video = $(".project-video");
+        this.title = $(".project-title");
+        this.subtitle = $(".project-subtitle");
+        this.category = $(".project-category");
+        this.role = $(".role");
+        this.description = $(".description");
+        this.festivalsSection = $(".project-festivals");
+        this.festivals = $(".festivals");
+        this.posterSection = $(".project-poster");
+        this.posterImage = $(".poster-image");
+        this.pressKitSection = $(".project-press-kit");
+        this.pressKitAnchorTag = $(".press-kit-anchor-tag");
+        this.rwSection = $(".project-related");
+        this.rwGrid = $(".related-work-grid");
 
         this.getData();
         this.setData();
 
         // Checks if project has related work and get data
-        if (Project.data[0].relatedWorkIDs.length > 1) {
+        if (Project.data[0].relatedWorkIDs.length > 0) {
             App.cons("Looking for " + Project.data[0].relatedWorkIDs.length + " related works");
 
             for (let i = 0; i < Project.data[0].relatedWorkIDs.length; i++) {
@@ -198,16 +188,19 @@ var Project = {
 
     setData: function () {
         App.cons("Page info has been changed");
+
+        // Replace video info
         if (Project.data[0].videoUrl) {
             let videoSrc = Project.data[0].videoUrl;
             let vimeoOptions = "?autoplay=1&color=bb0207&title=0&byline=0&portrait=0";
             Project.video.attr("src", videoSrc + vimeoOptions);
             Project.videoWrapper.addClass(Project.data[0].aspectRatio);
-            Project.videoWrapper.removeClass("display-none");
+            Project.videoSection.removeClass("display-none");
         } else {
-            Project.videoWrapper.addClass("display-none");
+            Project.videoSection.addClass("display-none");
         }
 
+        // Replace title info
         if (Project.data[0].title) {
             Project.title.text(Project.data[0].title);
             Project.title.removeClass("display-none");
@@ -215,7 +208,7 @@ var Project = {
             Project.title.addClass("display-none");
         }
 
-        App.cons("Project subtitle: " + Project.data[0].subtitle);
+        // Replace sutitle info
         if (Project.data[0].subtitle) {
             Project.subtitle.text(Project.data[0].subtitle);
             Project.subtitle.removeClass("display-none");
@@ -223,12 +216,22 @@ var Project = {
             Project.subtitle.addClass("display-none");
         }
 
+        // Replace category text info
         if (Project.data[0].category) {
-            Project.category.text(Project.data[0].category);
+            Project.category.text(Project.data[0].categoryText);
         } else {
             Project.category.addClass("display-none");
         }
 
+        // Replace role info
+        if (Project.data[0].role) {
+            Project.role.text("| " + Project.data[0].role + " |");
+            Project.role.removeClass("display-none");
+        } else {
+            Project.role.addClass("display-none");
+        }
+
+        // Replace description info
         if (Project.data[0].description) {
             Project.description.text(Project.data[0].description);
             Project.description.removeClass("display-none");
@@ -236,6 +239,7 @@ var Project = {
             Project.description.addClass("display-none");
         }
 
+        // Replace festivals info
         if (Project.data[0].festivals.length) {
             for (var i = 0; i < Project.data[0].festivals.length; i++) {
                 App.cons("Building festival list");
@@ -248,30 +252,32 @@ var Project = {
                 ul.append(li);
             }
 
-            Project.festivalsContainer.removeClass("display-none");
+            Project.festivalsSection.removeClass("display-none");
         } else {
-            Project.festivalsContainer.addClass("display-none");
+            Project.festivalsSection.addClass("display-none");
         }
 
+        // Replace poster info
         if (Project.data[0].posterUrl) {
             Project.posterImage.attr("src", Project.data[0].posterUrl);
-            Project.poster.removeClass("display-none");
+            Project.posterSection.removeClass("display-none");
         } else {
-            Project.poster.addClass("display-none");
+            Project.posterSection.addClass("display-none");
         }
 
+        // Replace press kit info
         if (Project.data[0].pressKitUrl) {
-            Project.pressKitLink.attr("src", Project.data[0].pressKitUrl);
-            Project.pressKit.removeClass("display-none");
+            Project.pressKitAnchorTag.attr("src", Project.data[0].pressKitUrl);
+            Project.pressKitSection.removeClass("display-none");
         } else {
-            Project.pressKit.addClass("display-none");
+            Project.pressKitSection.addClass("display-none");
         }
     },
 
     buildCards: function () {
         App.cons("Building related work cards");
         for (let i = 0; i < Project.relatedWork.length; i++) {
-            var newCard = new Card(Project.relatedWorkGrid, Project.relatedWork[i][0].id, "related-work-card", Project.relatedWork[i][0].title, Project.relatedWork[i][0].subtitle, Project.relatedWork[i][0].thumbUrl);
+            var newCard = new Card(Project.rwGrid, Project.relatedWork[i][0].id, "related-work-card", Project.relatedWork[i][0].title, Project.relatedWork[i][0].subtitle, Project.relatedWork[i][0].category, Project.relatedWork[i][0].thumbUrl);
             newCard.build();
         }
 
@@ -280,13 +286,13 @@ var Project = {
     },
 
     getCards: function () {
-        Project.relatedWorkCards = $(".related-work-card");
+        Project.rwCards = $(".related-work-card");
         gsap.set([".card-overlay", ".card-title", ".card-subtitle"], { autoAlpha: 0 });
     },
 
     addCardListeners: function () {
         App.cons("Adding event listeners to " + Project.relatedWork.length + " related work cards");
-        $.each(Project.relatedWorkCards, function (i) {
+        $.each(Project.rwCards, function (i) {
             $(this).on("click", Project.cardHandler);
             $(this).on("mouseenter", Project.hoverHandler);
             $(this).on("mouseleave", Project.outHandler);
@@ -294,19 +300,23 @@ var Project = {
     },
 
     hoverHandler: function (event) {
-        App.cons("Hover on " + this.id);
         let cardOverlay = $(this).find(".card-overlay");
         let cardTitle = $(this).find(".card-title");
         let cardSubtitle = $(this).find(".card-subtitle");
-        gsap.to([cardOverlay, cardTitle, cardSubtitle], { autoAlpha: 1 });
+        let thumbImage = $(this).find(".thumb-image");
+        gsap.set([cardOverlay, cardTitle, cardSubtitle], { autoAlpha: 0 });
+        gsap.to([cardOverlay], { duration: 0.4, autoAlpha: 1, ease: Power1.easeInOut });
+        gsap.to([cardTitle, cardSubtitle], { delay: 0.2, duration: 0.8, autoAlpha: 1, ease: Power1.easeInOut });
+        gsap.to([thumbImage], { duration: 0.4, scale: 1.02 });
     },
 
     outHandler: function (event) {
-        App.cons("Out");
         let cardOverlay = $(this).find(".card-overlay");
         let cardTitle = $(this).find(".card-title");
         let cardSubtitle = $(this).find(".card-subtitle");
+        let thumbImage = $(this).find(".thumb-image");
         gsap.to([cardOverlay, cardTitle, cardSubtitle], { autoAlpha: 0 });
+        gsap.to([thumbImage], { scale: 1 });
     },
 
     cardHandler: function (event) {
@@ -397,12 +407,13 @@ var DynamicData = {
     }
 }
 
-var Card = function (parentContainer, id, classSelector, title, subtitle, thumbUrl) {
+var Card = function (parentContainer, id, classSelector, title, subtitle, category, thumbUrl) {
     this.parentContainer = parentContainer;
     this.id = id;
     this.class = classSelector;
     this.title = title;
     this.subtitle = subtitle;
+    this.category = category;
     this.thumbUrl = thumbUrl;
 };
 
@@ -410,7 +421,7 @@ Card.prototype.build = function () {
     App.cons("Building card: " + this.id);
     let card = document.createElement("div");
     card.setAttribute("id", this.id);
-    card.className = "card " + this.class;
+    card.className = "card " + this.class + " " + this.category;
     this.parentContainer.append(card);
 
     let cardContent = document.createElement("div");
@@ -430,15 +441,19 @@ Card.prototype.build = function () {
     cardOverlay.className = "card-overlay";
     cardContent.append(cardOverlay);
 
+    let cardInfo = document.createElement("div");
+    cardInfo.className = "card-info";
+    cardContent.append(cardInfo);
+
     let cardTitle = document.createElement("h2");
     cardTitle.className = "card-title";
     cardTitle.innerHTML = this.title;
-    cardContent.append(cardTitle);
+    cardInfo.append(cardTitle);
 
     let cardSubtitle = document.createElement("h3");
     cardSubtitle.className = "card-subtitle";
     cardSubtitle.innerHTML = this.subtitle;
-    cardContent.append(cardSubtitle);
+    cardInfo.append(cardSubtitle);
 };
 
 
